0634585b67960138ac278c1db576a5acb789fc9c
Wouter <woutermartens94@gmail.com>
2019-12-13 10:31:01 +0100

    Class files according to UML

--- src/scripts/game.ts
+++ src/scripts/game.ts
@@ -1,31 +1,34 @@
-import 'phaser'
-import MainScene from './scenes/mainScene'
-import PreloadScene from './scenes/preloadScene'
+import 'phaser';
+import DemoScene from './scenes/mainScene';
+import PreloadScene from './scenes/preloadScene';
+import StartScene from './scenes/StartScene';
+import LevelScene from './scenes/LevelScene';
+import EndScene from './scenes/EndScene';
 
-const DEFAULT_WIDTH = 1280
-const DEFAULT_HEIGHT = 720
+const DEFAULT_WIDTH = 1280;
+const DEFAULT_HEIGHT = 720;
 
 // @ts-ignore https://github.com/photonstorm/phaser/issues/4522
 // still not working in 3.18.1 :/
 const config: GameConfig = {
-  backgroundColor: '#ffffff',
-  scale: {
-    parent: 'game',
-    mode: Phaser.Scale.FIT,
-    autoCenter: Phaser.Scale.CENTER_BOTH,
-    width: DEFAULT_WIDTH,
-    height: DEFAULT_HEIGHT
-  },
-  scene: [PreloadScene, MainScene],
-  physics: {
-    default: 'arcade',
-    arcade: {
-      debug: false,
-      gravity: { y: 400 }
-    }
-  }
+	backgroundColor: '#ffffff',
+	scale: {
+		parent: 'game',
+		mode: Phaser.Scale.FIT,
+		autoCenter: Phaser.Scale.CENTER_BOTH,
+		width: DEFAULT_WIDTH,
+		height: DEFAULT_HEIGHT
+	},
+	scene: [PreloadScene, DemoScene, StartScene, LevelScene, EndScene],
+	physics: {
+		default: 'arcade',
+		arcade: {
+			debug: true,
+			gravity: { y: 400 }
+		}
+	}
 };
 
 window.addEventListener('load', () => {
-  let game = new Phaser.Game(config);
+	let game = new Phaser.Game(config);
 });
---
+++ src/scripts/scenes/LevelScene.ts
@@ -0,0 +1,12 @@
+export default class LevelScene extends Phaser.Scene {
+    // square: Phaser.GameObjects.Rectangle;
+    // array: number[];
+
+    constructor() {
+      super({ key: 'LevelScene' });
+    }
+
+    create(): void {
+
+    }
+} 

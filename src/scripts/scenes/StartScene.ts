46a6b096009f58933cabcefcb3416fe1489128b3
Angie <ajbhuige@outlook.com>
2019-12-17 14:55:44 +0100

    Added custom font on startscene

--- src/scripts/scenes/StartScene.ts
+++ src/scripts/scenes/StartScene.ts
@@ -5,45 +5,51 @@ export default class StartScene extends Phaser.Scene {
     }
 
     init(): void {
+
+        // Initializer for keyboard input (S key)
         this.startKey = this.input.keyboard.addKey(
             Phaser.Input.Keyboard.KeyCodes.S
         );
         this.startKey.isDown = false;
     }
-
+ 
     preload(): void {
+
+        // Loads image for ghost
         this.load.image("ghost", "assets/img/snapghost.png");
+
+        // Loads image for title text
+        this.load.image("title", "assets/img/title.png");
+
+        // Loads image for background
         this.load.image("background", "assets/img/please.png"); //the background image for the scene
 
+        // Loads custom bitmap font
+        this.load.bitmapFont("dungeonFont", "assets/font/TheDungeonFont.png", "assets/font/TheDungeonFont.fnt");
+        
     }
 
     create(): void {
+        // Brick wall background
         const background = this.add.image(this.sys.game.canvas.width / 2, 0, "background")
         .setOrigin(0.5, 0);
 
-        // Hoe centeren?
-        const titleText = this.add.text(this.sys.game.canvas.width / 2, 50, "T.B.D. Dungeon",
-        {
-        fill: "white",
-        font: "100px candara",
-        })
+        // Title image
+        const title = this.add.image(this.sys.game.canvas.width / 2, 50, "title")
         .setOrigin(0.5, 0);
 
-        // Hoe centeren?
-        const buttonText = this.add.text(this.sys.game.canvas.width / 2, 600, "Klik op S om te beginnen",
-        {
-        fill: "white",
-        font: "30px candara"
-        })
+        // Subtitle
+        const buttonText = this.add.bitmapText(this.sys.game.canvas.width / 2, 600, "dungeonFont", "druk op S om te beginnen", 30)
         .setOrigin(0.5, 0);
 
-        // Add ghost image/button
+        // Add ghost image
         const ghost = this.add.image(this.sys.game.canvas.width / 2, 200, "ghost")
         .setOrigin(0.5, 0);
         ghost.setScale(0.4);
       }
 
       update(): void {
+          // Push S to start playing
           if (this.startKey.isDown) {
               this.scene.start("LevelScene");
           }

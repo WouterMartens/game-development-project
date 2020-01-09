dc2712587fd67119bb33388f59eeb4ce49c82c7b
Senenter <jage0030@hz.nl>
2020-01-09 14:37:17 +0100

    Update LevelScene2.ts
    Added Item and Enemy to levelscene2.ts

--- src/scripts/scenes/LevelScene2.ts
+++ src/scripts/scenes/LevelScene2.ts
@@ -1,13 +1,17 @@
 import Player from '../objects/Player';
 import Projectile from '../objects/ThumbsUp';
 import Door from '../objects/Door';
+import Enemy from '../objects/Enemy';
+import Item from '../objects/Pickup';
 
 
 export default class LevelScene2 extends Phaser.Scene {
 	private player: Player;
-	public enemies: Phaser.GameObjects.Group;
+	public enemies: Enemy;
 	private door: Door;
 	public hitDoor: boolean;
+	private items: Item;
+	
 
 	private levelSceneSound: any;
  
@@ -51,12 +55,27 @@ export default class LevelScene2 extends Phaser.Scene {
 		// Adds background image
 		const room1 = this.add.image(0, 0, 'room1').setOrigin(0, 0);
 
+		// Adds an item to the scene
+		this.items = new Item({
+			scene: this,
+			x: 200,
+			y: 500,
+			key: 'health'
+		});
+
+		this.enemies = new Enemy({
+			scene: this,
+			x: 500,
+			y: 200,
+			key: 'happy'
+		});
+
 		// Adds door to scene
 		this.door = new Door({
 			scene: this,
 			x: this.game.canvas.width / 2 + 8,
 			y: this.game.canvas.height / 4 - 110,
-			key: 'door',
+			key: 'door'
 		})
 		
 		// Set the state of hitDoor to false on level start
@@ -76,10 +95,14 @@ export default class LevelScene2 extends Phaser.Scene {
 		// this.physics.world.enableBody(this.player, 0);
 		this.physics.world.setBounds(80, 142 - this.player.height, this.sys.game.canvas.width - 80, this.sys.game.canvas.height - this.player.height, true, true, true, true);
 
+		this.physics.add.overlap(this.player, this.items, this.player.gainHealth);
+
 		const callback = () => {
 			console.log('hit the door');
 			this.hitDoor = true;
 		}
+
+		this.physics.add.overlap(this.player, this.items, this.player.gainHealth);
 
 		this.physics.add.collider(this.player, this.enemies, this.player.hit);
 		this.physics.add.overlap(this.player, this.door, callback);

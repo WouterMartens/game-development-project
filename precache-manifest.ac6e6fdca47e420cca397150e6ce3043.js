fc2190e47a58db93dabb0d8f8b2a2e9943afa402
Wouter <woutermartens94@gmail.com>
2020-01-10 16:36:27 +0100

    Initial dist subtree commit

---
+++ precache-manifest.ac6e6fdca47e420cca397150e6ce3043.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7062ac06bca06f6d63385e6a858f79dd",
+    "url": "assets/.DS_Store"
+  },
+  {
+    "revision": "1f05b5ad7165ced8d108ae1558ee8114",
+    "url": "assets/audio/bossTheme.mp3"
+  },
+  {
+    "revision": "a225b16fd1a4e13607b410718d570abe",
+    "url": "assets/audio/mainTheme.mp3"
+  },
+  {
+    "revision": "36b8b498b9bc280fc8a9e18d006e4dfe",
+    "url": "assets/font/TheDungeonFont.fnt"
+  },
+  {
+    "revision": "ea5f60b79056fd0e48aad969a0bd7168",
+    "url": "assets/font/TheDungeonFont.png"
+  },
+  {
+    "revision": "44d84dd503be06f7c1a553031094bfee",
+    "url": "assets/img/.DS_Store"
+  },
+  {
+    "revision": "89d4c68af2650ea245d2400a08ddb39a",
+    "url": "assets/img/angry.png"
+  },
+  {
+    "revision": "f99168b539f584896419ad3c88264093",
+    "url": "assets/img/businessMan.png"
+  },
+  {
+    "revision": "54c6e89281a2c908038c08e4c2bbf44d",
+    "url": "assets/img/businessManTextBox1.png"
+  },
+  {
+    "revision": "1559aab4ecd69ad58eb58d5b82e1d9c6",
+    "url": "assets/img/businessManTextBox2.png"
+  },
+  {
+    "revision": "b00c4b3ab093da62d9ed93e4bb911307",
+    "url": "assets/img/candy.png"
+  },
+  {
+    "revision": "a869d61104dbb3b1ccda6f4786eabbd4",
+    "url": "assets/img/door.png"
+  },
+  {
+    "revision": "eaec7c8d28cddea85b892a2432e04f23",
+    "url": "assets/img/dungeonbg.jpg"
+  },
+  {
+    "revision": "993d8f3021e19ba773da855505adbfbc",
+    "url": "assets/img/frowning.png"
+  },
+  {
+    "revision": "6ca09c3de2cb3e05dc63a53725a33b87",
+    "url": "assets/img/happy.png"
+  },
+  {
+    "revision": "f5f5d94d0d9b76d1f8085c987f04dae5",
+    "url": "assets/img/heart.png"
+  },
+  {
+    "revision": "c406b0e23ffdc271062826761df406f7",
+    "url": "assets/img/neutral.png"
+  },
+  {
+    "revision": "9b27f37efb1a92d3c1866a29e81eafe3",
+    "url": "assets/img/pedestrian.png"
+  },
+  {
+    "revision": "f44498c7ba7c3a1440f68f9d07012b76",
+    "url": "assets/img/pouting.png"
+  },
+  {
+    "revision": "5008e0d5eb10ea93fdc2cd4f353dd48b",
+    "url": "assets/img/room-1.png"
+  },
+  {
+    "revision": "d5d8de2ed30023a8c13dacccfef76776",
+    "url": "assets/img/slightlysmilingface.png"
+  },
+  {
+    "revision": "19b1768da71f165e9f0f39674dd2f66c",
+    "url": "assets/img/snapghost.png"
+  },
+  {
+    "revision": "db9af71f275e702fb212a79994fd67f6",
+    "url": "assets/img/startBackground.png"
+  },
+  {
+    "revision": "cddd66ca7e101f3712325eb732b5fdc5",
+    "url": "assets/img/thumbs-up.png"
+  },
+  {
+    "revision": "616be74ec76fdbe3d1b0e7433d9be35f",
+    "url": "assets/img/torch-front-spritesheet.png"
+  },
+  {
+    "revision": "c658205110152e285fbf373dd9e99f32",
+    "url": "assets/img/torch-side-spritesheet.png"
+  },
+  {
+    "revision": "57040e5677322118f6d56a1d9e43c5c6",
+    "url": "favicon.ico"
+  },
+  {
+    "revision": "18f8227b214cf559b36fb59f589b2607",
+    "url": "icons/favicon.ico"
+  },
+  {
+    "revision": "d1c98b6c505397a470016c4464fe6d75",
+    "url": "icons/icons-192.png"
+  },
+  {
+    "revision": "53f4ee16368bd31d3e200c665064f941",
+    "url": "icons/icons-512.png"
+  },
+  {
+    "revision": "685a726fd4504f99eba611003c90bf1f",
+    "url": "index.html"
+  },
+  {
+    "revision": "cc66b2d28e06f33089f0",
+    "url": "main.4e164285d4c4fd5a7642.bundle.js"
+  },
+  {
+    "revision": "ff0d08eca92dee1c2fc3a94297f58aeb",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "sw.js"
+  },
+  {
+    "revision": "77f3b31858941e6bbdbc",
+    "url": "vendors.ea55fd457f056af9d556.bundle.js"
+  }
+]);
--- sw.js
+++ sw.js
@@ -1,3 +1,5 @@
+importScripts("precache-manifest.ac6e6fdca47e420cca397150e6ce3043.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
 /**
  * You should only modify this, if you know what you are doing.
  * This phaser template is using workbox (https://developers.google.com/web/tools/workbox/)
@@ -6,3 +8,4 @@
  * webpack/webpack.common.js
  */
 workbox.precaching.precacheAndRoute(__precacheManifest)
+
